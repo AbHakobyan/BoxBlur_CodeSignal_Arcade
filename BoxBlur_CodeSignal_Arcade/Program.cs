895256345386926e9a03045cec56090b814ceebe
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-25 10:41:36 +0400

    Добавьте файлы проекта.

---
+++ BoxBlur_CodeSignal_Arcade/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxBlur_CodeSignal_Arcade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] array = ;
+        }
+
+        static int[][] boxBlur(int[][] image)
+        {
+            int x = image[0].Length - 2;
+            int y = image.Length - 2;
+            int[][] b = new int[y][];
+
+            for (int i = 0; i < y; i++)
+            {
+                b[i] = new int[x];
+                for (int j = 0; j < x; j++)
+                {
+                    b[i][j] = image[i][j] + image[i][j + 1] + image[i][j + 2] +
+                              image[i + 1][j] + image[i + 1][j + 1] + image[i + 1][j + 2] +
+                              image[i + 2][j] + image[i + 2][j + 1] + image[i + 2][j + 2];
+                    b[i][j] /= 9;
+                }
+            }
+            return b;
+        }
+    }
+}
---
+++ BoxBlur_CodeSignal_Arcade/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("BoxBlur_CodeSignal_Arcade")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BoxBlur_CodeSignal_Arcade")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("00f17e3e-93d6-45a0-b744-76d0da2d9943")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
